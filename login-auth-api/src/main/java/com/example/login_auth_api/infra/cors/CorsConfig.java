eb683a2a9b7ec767226b62ee68126ade5d76bcb8
mylenaverspeelt <mylena49@hotmail.com>
2024-07-11 08:35:19 -0300

    fix: cors method

--- login-auth-api/src/main/java/com/example/login_auth_api/infra/cors/CorsConfig.java
+++ login-auth-api/src/main/java/com/example/login_auth_api/infra/cors/CorsConfig.java
@@ -7,9 +7,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
-	@Override
-	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**").allowedOrigins("https://move-it-taupe-nu.vercel.app").allowedMethods("GET", "POST")           .allowedHeaders("*")
-        .allowCredentials(true);
-	}
+	  @Override
+	    public void addCorsMappings(CorsRegistry registry) {
+	        registry.addMapping("/**")
+	                .allowedOrigins("https://move-it-taupe-nu.vercel.app")
+	                .allowedMethods("GET", "POST", "OPTIONS")
+	                .allowedHeaders("*")
+	                .allowCredentials(true);
+	    }
 }
