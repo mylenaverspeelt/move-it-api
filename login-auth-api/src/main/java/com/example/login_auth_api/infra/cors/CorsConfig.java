e599e4b56bd069a042f7a6b356974350d78eed93
mylenaverspeelt <mylena49@hotmail.com>
2024-07-11 08:04:42 -0300

    fix: cors path

--- login-auth-api/src/main/java/com/example/login_auth_api/infra/cors/CorsConfig.java
+++ login-auth-api/src/main/java/com/example/login_auth_api/infra/cors/CorsConfig.java
@@ -9,6 +9,6 @@ public class CorsConfig implements WebMvcConfigurer {
 
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**").allowedOrigins("http://localhost:4200").allowedMethods("GET", "POST");
+		registry.addMapping("/**").allowedOrigins("https://move-it-taupe-nu.vercel.app").allowedMethods("GET", "POST");
 	}
 }
